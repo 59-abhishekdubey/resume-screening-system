0493d71d035bfbbeb783d221fd32eeb9202edbad
Abhishek Dubey <abhishekdubey112jeem@gmail.com>
2026-09-20 12:22:47 +0530

    chore: remove scratch files and pycache from tracking

--- tmp_read.py
+++
@@ -1,8 +0,0 @@
-import csv
-with open(r'C:\Users\Lenovo\OneDrive\Desktop\FUTURE_int\resume-screening-system\data\cleaned_resumes.csv') as f:
-    reader = csv.reader(f)
-    for i, row in enumerate(reader):
-        if i < 5:
-            print(row)
-        else:
-            break
